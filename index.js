47c5185c32b59a014b40cfc2f45677444d72e298
jsolisu <jcsolisu2000@gmail.com>
2018-03-11 16:15:29 -0600

    :art:Lint

--- index.js
+++ index.js
@@ -5,84 +5,83 @@
 
 console.log(`github-clone-all-org (GitHub) version ${require('./package.json').version}\n\r(c) 2018 JSolisU. MIT License.\n\r`);
 const options = require('yargs')
-    .usage('Usage: $0 [options]')
-    .alias('o', 'org')
-    .describe('o', 'Organization')
-    .alias('u', 'usr')
-    .describe('u', 'GitHub username')
-    .alias('p', 'pwd')
-    .describe('p', 'GitHub password')
-    .alias('d', 'dest')
-    .describe('d', 'Destination path')
-    .help('h')
-    .demandOption(['o', 'u', 'p'])
-    .argv;
+  .usage('Usage: $0 [options]')
+  .alias('o', 'org')
+  .describe('o', 'Organization')
+  .alias('u', 'usr')
+  .describe('u', 'GitHub username')
+  .alias('p', 'pwd')
+  .describe('p', 'GitHub password')
+  .alias('d', 'dest')
+  .describe('d', 'Destination path')
+  .help('h')
+  .demandOption(['o', 'u', 'p'])
+  .argv;
 
 const github = require('octonode');
 const client = github.client({
-    username: options.usr,
-    password: options.pwd
+  username: options.usr,
+  password: options.pwd
 });
 const childProcess = require('child_process');
 const rimraf = require('rimraf');
 const path = require('path');
 const fs = require('fs');
 
-const ghme = client.me();
 const ghorg = client.org(options.org);
 
 let rootPath = process.cwd();
 if (options.dest) {
-    if (fs.existsSync(options.dest)) {
-        rootPath = options.dest;
-    } else {
-        console.log(`Path <${options.dest}> not found.`);
-        process.exit(-1);
-    }
+  if (fs.existsSync(options.dest)) {
+    rootPath = options.dest;
+  } else {
+    console.log(`Path <${options.dest}> not found.`);
+    process.exit(-1);
+  }
 }
 
 client.get('/user', {}, function (err, status, body, headers) {
-    console.log(`Welcome ${body.name}\n\r`);
+  console.log(`Welcome ${body.name}\n\r`);
 
-    ghorg.info((err, data, header) => {
-        console.log(`Info for [${data.login}] organization:`);
-        console.log(`* Description: ${data.description}`);
-        console.log(`* Url: ${data.html_url}`);
-        console.log(`* Total private repositories: ${data.total_private_repos}`);
-        console.log(`* Plan: ${data.plan.name}`);
-        console.log(`* Plan seats: ${data.plan.seats}`);
-        console.log(`* Plan filled seats: ${data.plan.filled_seats}`);
-        console.log(`* Default repository permission: ${data.default_repository_permission}`);
-        console.log(`* Members can create repositories: ${data.members_can_create_repositories}`);
-    });
+  ghorg.info((err, data, header) => {
+    console.log(`Info for [${data.login}] organization:`);
+    console.log(`* Description: ${data.description}`);
+    console.log(`* Url: ${data.html_url}`);
+    console.log(`* Total private repositories: ${data.total_private_repos}`);
+    console.log(`* Plan: ${data.plan.name}`);
+    console.log(`* Plan seats: ${data.plan.seats}`);
+    console.log(`* Plan filled seats: ${data.plan.filled_seats}`);
+    console.log(`* Default repository permission: ${data.default_repository_permission}`);
+    console.log(`* Members can create repositories: ${data.members_can_create_repositories}`);
+  });
 
-    ghorg.repos((err, data, header) => {
-        console.log('\n\rRepositories:\n\r');
-        data.forEach(repository => {
-            let ghrepo = client.repo(`${options.org}/${repository.name}`);
-            ghrepo.branches((err, data, header) => {
-                data.forEach(branch => {
-                    console.log(`${repository.name} => ${repository.html_url} (${branch.name})`);
-                    let repoURL = `https://${options.usr}:${options.pwd}@github.com/${options.org}/${repository.name}.git`;
-                    let destPath = path.join(rootPath, `${options.org}_${repository.name}_${branch.name}`);
+  ghorg.repos((err, data, header) => {
+    console.log('\n\rRepositories:\n\r');
+    data.forEach(repository => {
+      let ghrepo = client.repo(`${options.org}/${repository.name}`);
+      ghrepo.branches((err, data, header) => {
+        data.forEach(branch => {
+          console.log(`${repository.name} => ${repository.html_url} (${branch.name})`);
+          let repoURL = `https://${options.usr}:${options.pwd}@github.com/${options.org}/${repository.name}.git`;
+          let destPath = path.join(rootPath, `${options.org}_${repository.name}_${branch.name}`);
 
-                    // cleanup branch
-                    rimraf.sync(destPath);
+          // cleanup branch
+          rimraf.sync(destPath);
 
-                    childProcess.execFileSync('git', ['clone', repoURL, destPath], {
-                        env: process.env
-                    });
+          childProcess.execFileSync('git', ['clone', repoURL, destPath], {
+            env: process.env
+          });
 
-                    // If the branch is master, it is already cloned
-                    if (branch.name != 'master') {
-                        process.chdir(destPath);
-                        childProcess.execFileSync('git', ['checkout', branch.name], {
-                            env: process.env
-                        });
-                        process.chdir(rootPath);
-                    }
-                });
+          // If the branch is master, it is already cloned
+          if (branch.name !== 'master') {
+            process.chdir(destPath);
+            childProcess.execFileSync('git', ['checkout', branch.name], {
+              env: process.env
             });
+            process.chdir(rootPath);
+          }
         });
+      });
     });
+  });
 });
