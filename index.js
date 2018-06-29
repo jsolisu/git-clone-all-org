8890c82f07e9ddba2e53af23c869654b659e3184
jsolisucads <jsolis@cads.com.gt>
2018-06-28 18:56:49 -0600

    Add log support

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const options = require('yargs')
   .describe('d', 'Destination path (-d "c:\\temp")')
   .alias('c', 'clean')
   .describe('c', 'Clean destination path')
+  .alias('l', 'log')
+  .describe('l', 'Generate log')
   .help('h')
   .demandOption(['o', 'u', 'p'])
   .argv;
@@ -42,6 +44,13 @@ function fixPath (pathToFix) {
 }
 
 let rootPath = fixPath(process.cwd());
+
+function convertUnixDateTime (datetime) {
+  let d = new Date(datetime * 1000);
+  let utc = d.getTime() + (d.getTimezoneOffset() * 60000); // This converts to UTC 00:00
+  let nd = new Date(utc + (3600000 * 6));
+  return nd.toLocaleString();
+}
 
 function checkForGit () {
   return commandExists('git')
@@ -101,7 +110,14 @@ function getOrgInfo () {
 }
 
 function getRepositories () {
+  const logFile = path.join(rootPath, 'github_clone_all_org.log');
+
   return new Promise((resolve, reject) => {
+    var file;
+    if (options.log) {
+      file = fs.openSync(logFile, 'w');
+    }
+
     // Clean destination path?
     if (options.dest && options.clean) {
       fs.readdir(rootPath, function (err, files) {
@@ -144,13 +160,20 @@ function getRepositories () {
                   });
 
                   // If the branch is master, it is already cloned
+                  process.chdir(destPath);
                   if (branch.name !== 'master') {
-                    process.chdir(destPath);
                     childProcess.execFileSync('git', ['checkout', branch.name], {
                       env: process.env
                     });
-                    process.chdir(rootPath);
                   }
+
+                  // Generate log
+                  if (options.log) {
+                    let res = childProcess.execFileSync('git', ['log', '-1', '--pretty=format:%cI,%an,%ae,%s,%b']).toString().split(',');
+                    fs.writeSync(file, `Repository: ${repository.name}, Branch: ${branch.name}, Last commit: ${new Date(res[0]).toString()} by -${res[1]}-\n\r`);
+                  }
+
+                  process.chdir(rootPath);
                 });
               }
               resolve();
@@ -158,6 +181,9 @@ function getRepositories () {
           }));
         });
         p.then(() => {
+          if (options.log) {
+            fs.closeSync(file);
+          }
           resolve(data);
         });
       }
